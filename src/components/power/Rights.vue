6196d3fae1771630a0f88adf28526cdcd18ce632
pick梦瑶 <1490801829@qq.com>
2020-08-30 17:05:54 +0800

    完成了用户列表的相关功能

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -5,19 +5,43 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
-  <!-- 卡片 -->
-   <el-card>
-     3212
-   </el-card>
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-table :data="rightList" stripe border>
+        <el-table-column type="index" label="#" ></el-table-column>
+        <el-table-column prop="authName" label="权限名称"></el-table-column>
+        <el-table-column prop="path" label="路径"></el-table-column>
+        <el-table-column label="权限等级">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level==='0'">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level==='1'">二级</el-tag>
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 <script>
 export default {
   props: [],
   data() {
-    return {}
+    return {
+      // 权限管理
+      rightList: []
+    }
   },
-  methods: {}
+  created() {
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) return this.$mssage.error(res.data.msg)
+      this.rightList = res.data
+      console.log(this.rightList)
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
