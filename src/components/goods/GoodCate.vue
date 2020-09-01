fe31d6449d95b4a5d32ad68add16590477e1f842
pick梦瑶 <1490801829@qq.com>
2020-09-01 17:07:36 +0800

    在不上去就 绝望了

---
+++ src/components/goods/GoodCate.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      querInfo: {
+        type: 3, // 分类
+        pagenum: 1,
+        pagesize: 2
+      },
+      // 商品分类的数据列表 默认为空
+      cateList: [],
+      total: 0 // 总数据
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    //   获取商品分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.querInfo
+      })
+      this.cateList = res.data.result
+      this.total = res.data.total
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,7 @@ import Welcome from '../components/welcome.vue'
 import User from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Role.vue'
+import GoodCate from '../components/goods/GoodCate.vue'
 
 Vue.use(VueRouter)
 
@@ -20,7 +21,8 @@ const routes = [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: User },
       { path: '/rights', component: Rights },
-      { path: '/roles', component: Roles }
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: GoodCate }
     ]
   }
 ]
